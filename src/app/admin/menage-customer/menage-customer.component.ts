8217aba163822d667eda8c43a489ebacca58583a
manuel crecchia <manuelcrecchia054@gmail.com>
2024-07-18 20:41:12 +0200

    provato qualcosa

--- src/app/admin/menage-customer/menage-customer.component.ts
+++ src/app/admin/menage-customer/menage-customer.component.ts
@@ -3,6 +3,15 @@ import { GlobalService } from '../../service/global.service';
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import {CustomerModelService} from '../../service/customer-model.service'
+import { Injectable } from '@angular/core';
+import {  OnInit } from '@angular/core';
+
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
 
 
 @Component({
@@ -11,21 +20,26 @@ import {CustomerModelService} from '../../service/customer-model.service'
   styleUrl: './menage-customer.component.css'
 })
 export class MenageCustomerComponent {
-  
+
   constructor(public customerModelService: CustomerModelService, private http: HttpClient, private globalService: GlobalService, private router: Router) {}
 
 
 
 
+ngOnInit() {
+  this.customerModelService = new CustomerModelService();
+}
 
 
 
+  // Dichiarazione di variabili per i dati
+     servizi = {
+servizi: ['Servizi1', 'Servizio 2', 'Servizio 3'], // Esempio di array servizi
+interventi: ['Interventi1', 'Intervento 2'] // Esempio di array interventi
 
+};
 
-
-
-
-
+// Altri metodi o variabili se necessario
 
 
 
@@ -33,7 +47,7 @@ export class MenageCustomerComponent {
 
   conferm(numeroPreventivo: string){
     const body = { numeroPreventivo: numeroPreventivo };
-  
+
     this.http
     .post(
       this.globalService.url + 'quotes/conferm',
@@ -46,7 +60,7 @@ export class MenageCustomerComponent {
           this.router.navigateByUrl('/')
         }
       })
-  
+
       }
   }
 
