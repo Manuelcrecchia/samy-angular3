78c80bcb399256bd4036f5e32ce800c47553c08d
Alessio Veratti <verattialessio@gmail.com>
2024-11-26 17:15:39 +0100

    Sistemato il calendario e aggiunto il backup del db

--- src/app/admin/calendar/calendar-home/calendar-home.component.ts
+++ src/app/admin/calendar/calendar-home/calendar-home.component.ts
@@ -97,10 +97,10 @@ export class CalendarHomeComponent {
       });
   }
 
-   convertDateToICSFormat(date: Date) {
+  convertDateToICSFormat(date: Date) {
     // Crea un oggetto Date a partire dalla stringa fornita
     const parsedDate = new Date(date);
-  
+
     // Estrai le componenti della data
     const year = parsedDate.getUTCFullYear();
     const month = String(parsedDate.getUTCMonth() + 1).padStart(2, '0'); // I mesi sono zero-indexed, quindi +1
@@ -108,7 +108,7 @@ export class CalendarHomeComponent {
     const hours = String(parsedDate.getUTCHours()).padStart(2, '0');
     const minutes = String(parsedDate.getUTCMinutes()).padStart(2, '0');
     const seconds = String(parsedDate.getUTCSeconds()).padStart(2, '0');
-  
+
     // Crea la stringa nel formato desiderato
     return `${year}${month}${day}T${hours}${minutes}${seconds}Z`;
   }
@@ -123,7 +123,6 @@ export class CalendarHomeComponent {
       : null;
   }
 
-
   onAppointmentFormOpening(e: any) {
     const form = e.form;
 
@@ -134,14 +133,13 @@ export class CalendarHomeComponent {
     toolbarItems.forEach((item: any) => {
       if (item.shortcut === 'cancel') {
         this.currentDate = new Date();
-  this.currentView = 'month';
-  this.recurrenceRuleisVisible = false;
-  this.saveRecurrenceRule = '';
-          form.itemOption('recurrenceRule', 'visible', false);
-        };
-      
+        this.currentView = 'month';
+        this.recurrenceRuleisVisible = false;
+        this.saveRecurrenceRule = '';
+        form.itemOption('recurrenceRule', 'visible', false);
+      }
     });
-  
+
     // Assicurati di aggiornare la toolbar con le modifiche
     popup.option('toolbarItems', toolbarItems);
 
@@ -212,25 +210,39 @@ export class CalendarHomeComponent {
         },
 
         onValueChanged: (args: any) => {
-          console.log(args)
-          if (args.value != null && args.value.startsWith('FREQ=WEEKLY;')) {
-              const dayOfWeek = this.selectedDate.getDay();
-              console.log(args.value)
-
-              const recurrenceEditor = args.component;
-              this.saveRecurrenceRule = args.value;
-              recurrenceEditor.option(
-                'value',
-                `FREQ=WEEKLY;BYDAY=${
-                  ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'][dayOfWeek]
-                }`
-              );
-              
-          }else{
-            this.saveRecurrenceRule = ''
-
+        
+          // Gestione per la ricorrenza settimanale
+          if (
+            args.value != null && 
+            args.value.startsWith('FREQ=WEEKLY;') && 
+            (args.previousValue == null || (args.previousValue != null && !args.previousValue.startsWith('FREQ=WEEKLY;')))
+          ) {
+            const dayOfWeek = this.selectedDate.getDay();
+            const recurrenceEditor = args.component;
+            this.saveRecurrenceRule = args.value;
+            recurrenceEditor.option(
+              'value',
+              `FREQ=WEEKLY;BYDAY=${['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'][dayOfWeek]}`
+            );
+          } 
+          // Gestione per la ricorrenza mensile
+          else if (
+            args.value != null && 
+            args.value.startsWith('FREQ=MONTHLY;') && 
+            (args.previousValue == null || (args.previousValue != null && !args.previousValue.startsWith('FREQ=MONTHLY;')))
+          ) {
+            const dayOfMonth = this.selectedDate.getDate();
+            const recurrenceEditor = args.component;
+            this.saveRecurrenceRule = args.value;
+            recurrenceEditor.option(
+              'value',
+              `FREQ=MONTHLY;BYMONTHDAY=${dayOfMonth}`
+            );
+          } 
+          // Altrimenti, aggiorna solo la regola di ricorrenza salvata
+          else {
+            this.saveRecurrenceRule = args.value;
           }
-
         },
         onInitialized: (args: any) => {
           this.saveRecurrenceRule = 'FREQ=DAILY';
@@ -310,7 +322,7 @@ export class CalendarHomeComponent {
       body.title == undefined ||
       body.startDate == undefined ||
       body.endDate == undefined ||
-      body.categories == undefined
+      body.categories == undefined 
     ) {
       this.popup.text = 'Compilare tutti i campi obbligatori';
       this.popup.openPopup();
@@ -358,26 +370,61 @@ export class CalendarHomeComponent {
       categories: e.newData['categories'],
       recurrenceException: e.newData['recurrenceException'],
     };
-    this.http
-      .post(this.globalService.url + 'appointments/edit', body, {
-        headers: this.globalService.headers,
-        responseType: 'text',
-      })
-      .subscribe((response) => {
-        this.ngOnInit();
-      });
+    if (
+      body.title == undefined ||
+      body.startDate == undefined ||
+      body.endDate == undefined ||
+      body.categories == undefined     ) {
+      this.popup.text = 'Compilare tutti i campi obbligatori';
+      this.popup.openPopup();
+      this.ngOnInit();
+    } else {
+      this.http
+        .post(this.globalService.url + 'appointments/edit', body, {
+          headers: this.globalService.headers,
+          responseType: 'text',
+        })
+        .subscribe((response) => {
+          this.ngOnInit();
+          if (body.categories == 'sopralluogo') {
+            this.http
+              .post(
+                this.globalService.url +
+                  'appointments/sendInspectionConfirmation',
+                body,
+                {
+                  headers: this.globalService.headers,
+                  responseType: 'text',
+                }
+              )
+              .subscribe((response) => {
+                if (response == 'NO') {
+                  this.popup.text =
+                    "Non è stato possibile inviare la mail di conferma dell'appuntamento perchè non è presente nessuna mail associata al preventivo";
+                  this.popup.openPopup();
+                }
+              });
+          }
+        });
+    }
   }
 
   onAppointmentDeletedQuestion(e: AppointmentDeletedEvent) {
     const appointmentData = e.appointmentData;
-    console.log(appointmentData)
-    // Controlla se l'evento ha una regola di ricorrenza
-    if (appointmentData.recurrenceRule && appointmentData.recurrenceRule.trim() !== '') {
-        // Se ha una regola di ricorrenza, chiama appointmentsDeleted
-        this.appointmentsDeleted(e);
+   // Controlla se l'evento ha una regola di ricorrenza
+    if (
+      appointmentData.recurrenceRule &&
+      appointmentData.recurrenceRule.trim() !== ''
+    ) {
+      // Se ha una regola di ricorrenza, chiama appointmentsDeleted
+      this.appointmentsDeleted(e);
     } else {
-        // Altrimenti, chiama appointmentDeleted
-        this.appointmentDeleted(e);
+      if(appointmentData.recurrenceRule == ""){
+        this.appointmentsDeleted(e);
+
+      }else{
+      // Altrimenti, chiama appointmentDeleted
+      this.appointmentDeleted(e);}
     }
   }
 
