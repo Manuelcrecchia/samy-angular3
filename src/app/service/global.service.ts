04d3c72c0624c9393fdb6e3424bc9a9fefca36cb
Manuel Crecchia <manuelcrecchia054@gmail.com>
2025-03-08 16:05:43 +0100

    sistemati preventivi

--- src/app/service/global.service.ts
+++ src/app/service/global.service.ts
@@ -17,7 +17,7 @@ export class GlobalService {
 
   //url = "https://samipulizie.it:4000/";
 
-  url = "http://192.168.1.8:5000/";
+  url = "http://192.168.1.29:5000/";
 
   //https://betterprogramming.pub/how-to-convert-your-angular-application-to-a-native-mobile-app-android-and-ios-c212b38976df
 
