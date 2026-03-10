63e7285822a85768ad6ea55b6f3d33fbcbadf444
manuel crecchia <manuelcrecchia054@gmail.com>
2026-03-10 22:57:50 +0100

    sistemato calendario e turni

--- src/app/service/global.service.ts
+++ src/app/service/global.service.ts
@@ -23,10 +23,13 @@ export class GlobalService {
     }
 
     if (this.tenantService.isEmmeci) {
-      return 'https://nodeemmeci.mvtechcore.it/';
+      //return 'https://nodeemmeci.mvtechcore.it/';
+            return 'http://nodeemmeci.mvtechcore.it:5001/';
+
     }
 
-    return 'https://nodesami.mvtechcore.it/';
+    //return 'https://nodesami.mvtechcore.it/';
+    return 'http://nodesami.mvtechcore.it:5001/';
   }
 
   checkVersion(): Promise<boolean> {
